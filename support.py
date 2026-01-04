067fc0654bc26ab964f166d6def7feaf58572b94
Quacky <66437768+quacky20@users.noreply.github.com>
2026-01-05 00:42:34 +0530

    initial setup

---
+++ support.py
@@ -0,0 +1,14 @@
+from os import walk
+from os.path import join
+from settings import *
+
+def import_folder(*path):
+    surface_list = []
+    
+    for _, _, file_names in walk(join(*path)):
+        for file_name in sorted(file_names, key = lambda name: int(name.split('.')[0])):
+            full_path = join(*path, file_name)
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surf)
+            
+    return surface_list
